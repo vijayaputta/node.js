76bae1b8382dfd597454eac54096838bdecc1bdf
vijayaputta <151710074+vijayaputta@users.noreply.github.com>
2024-12-30 16:17:16 +0530

    Add files via upload

---
+++ NodePractice/math.js
@@ -0,0 +1,13 @@
+//const addition = (a, b) => a+b;
+//const substraction = (a, b) => a-b;
+//const multiplication = (a, b) => a*b;
+//const division = (a, b) => a/b;
+//const remainder = (a, b) => a%b;
+//module.exports= {addition,substraction,multiplication,division,remainder};
+
+
+//exports.addition=(a, b) => a+b;
+//exports.substraction=(a, b) => a-b;
+//exports.multiplication=(a, b) => a*b;
+//exports.division=(a, b) => a/b;
+//exports.remainder=(a, b) => a%b;
---
+++ NodePractice/server.js
@@ -0,0 +1,54 @@
+//console.log('Hello Sasi')
+//console.log('Hello Sasi')
+//console.log(global)
+
+//OS module
+//const os = require('os');
+//console.log(os.type());
+//console.log(os.version());
+//console.log(os.homedir());
+//console.log(__dirname());
+//console.log(__filename());
+
+//path module
+//const os = require('os');
+//const path = require('path');
+//console.log(path.dirname(__filename));
+//console.log(path.basename(__filename));
+//onsole.log(path.extname(__filename));
+//console.log(path.parse(__filename));
+
+//math module
+//const math = require('./math');
+//console.log(math.addition(4,7));
+//console.log(math.substraction(4,7));
+//console.log(math.multiplication(4,7));
+//console.log(math.division(4,7));
+//console.log(math.remainder(4,7));
+
+//const{addition,multiplication}=require('./math');
+//console.log(addition(22,7));
+//console.log(multiplication(22,7));
+
+//const math = require('./math');
+//console.log(math.addition(20,30));
+//console.log(math.substraction(20,30));
+//console.log(math.multiplication(20,30));
+//console.log(math.division(20,30));
+//console.log(math.remainder(20,30));
+
+//const http = require('http');
+//http.createServer(req,res) => {
+    //res.write('Hi, We are practicing node');
+    //res.end();
+    //console.log('server is running...')
+//}).listen(8080)
+
+
+
+//http=require('http');
+//http.createServer(req, res)=>{
+    //res.write(req.url);
+    //res.end();
+    //console.log('server is running...')
+//}).listen(8080)
